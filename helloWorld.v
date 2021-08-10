1a2ced1584eb9e792a47894af5ead16eca8fc3a8
Yashwanth Vernekar <yashvernekar674@gmail.com>
2021-08-10 13:58:55 +0530

    Code for "Hello World" print to the console.

---
+++ helloWorld.v
@@ -0,0 +1,10 @@
+// Basic Hello world code
+/* 
+This code is uses the "$display" to display A basic "Hello World" print to the console.
+*/
+module hello();
+ initial begin
+   $display("Hello World");
+ end
+endmodule
+ 
